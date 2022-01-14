f3f8fc0a3eedd543a47a0059c1e498397dcc3268
tomgxz <75934344+Tomgxz@users.noreply.github.com>
2022-01-14 23:09:23 +0000

    More work on info page

--- assets/js/info/main.js
+++ assets/js/info/main.js
@@ -1,57 +1,4 @@
 $(document).ready(function () {
-    letterWrapper(".globalnav .globalnav-link-text");
-
-    gsap.utils.toArray(".globalnav .globalnav-content").forEach((item) => {
-        item.style.opacity=1;jsE(item);
-    });
-
-
-    ScrollTrigger.matchMedia({
-        "(min-width: 681px)": function() {
-            gsap.utils.toArray(".globalnav .globalnav-link-text .letter, .globalnav .globalnav-link-text:before").forEach((item) => {item.style.opacity=0;item.style.translateX=40;jsE(item);
-            });
-
-            gsap.utils.toArray(".globalnav .globalnav-link-home svg").forEach((item) => {
-                item.style.opacity=0;item.style.translateY=60;jsE(item);
-            });
-
-            anime.timeline({loop:false})
-                .add({
-                    targets:".globalnav .globalnav-link-home svg",
-                    translateY:[60,0],
-                    opacity:[0,1],
-                    easing:"easeOutQuint",
-                    duration:1000,
-                })
-
-                .add({
-                    targets:".globalnav .globalnav-link-text .letter",
-                    translateX: [40,0],
-                    opacity: [0,1],
-                    easing: "easeOutQuint",
-                    duration: 600,
-                    offset:"-=200",
-                    delay: (el, i) => 50 * i,
-                });
-
-            jsQ(".globalnav .globalnav-link-home svg");
-            jsQ(".globalnav .globalnav-link-text .letter");
-        },
-
-    });
-
-    gsap.to(".globalnav .globalnav-content", {css:{opacity:0},
-        scrollTrigger: {
-            trigger:".globalnav",
-            start:"top top",
-            end:"+=48",
-            ease:"power2.in",
-            markers:false,
-            scrub:true,
-        }
-    });
-
-    jsQ(".globalnav .globalnav-content");
 
     anime({
         targets: '.section.one .word-cloud-container svg path',
@@ -61,7 +8,7 @@ $(document).ready(function () {
         delay: (el,i) =>  100 * i,
     });
 
-    gsap.utils.toArray(".horizontal-section-container").forEach((item) => {
+    gsap.utils.toArray(".horizontal-section-container.one").forEach((item) => {
 
         gsap.to(item, {
             xPercent: -100, 
@@ -70,7 +17,41 @@ $(document).ready(function () {
             scrollTrigger: {
                 trigger: item,
                 start: "top top",
-                end: "+=%".replace("%",innerWidth * 2),
+                end: "+=%".replace("%",innerWidth),
+                scrub: true,
+                pin: true,
+                anticipatePin: 1
+            }
+        });
+    });
+
+    gsap.utils.toArray(".horizontal-section-container.two").forEach((item) => {
+
+        gsap.to(item, {
+            xPercent: -100, 
+            x: () => innerWidth,
+            ease: "none",
+            scrollTrigger: {
+                trigger: item,
+                start: "top top",
+                end: "+=%".replace("%",innerWidth * 4),
+                scrub: true,
+                pin: true,
+                anticipatePin: 1
+            }
+        });
+    });
+
+    gsap.utils.toArray(".horizontal-section-container.three").forEach((item) => {
+
+        gsap.to(item, {
+            xPercent: -100, 
+            x: () => innerWidth,
+            ease: "none",
+            scrollTrigger: {
+                trigger: item,
+                start: "top top",
+                end: "+=%".replace("%",innerWidth * 3),
                 scrub: true,
                 pin: true,
                 anticipatePin: 1
